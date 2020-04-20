393d9e95862992850f79793ff7f27b45b5885027
Marty Ballard <marty331@gmail.com>
2020-04-20 06:43:48 -0500

    initial commit

---
+++ src/MoviesList.js
@@ -0,0 +1,49 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import Movie from './Movie';
+
+const API_KEY = process.env.REACT_APP_MD_KEY;
+
+
+class MoviesList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=20`);
+      console.log('movie lisst ', res);
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results,
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    console.log(this.state.movies);
+    return (
+      <MovieGrid>
+        {this.state.movies.map((movie) => <Movie movie={movie} key={movie.id} />)}
+      </MovieGrid>
+
+
+    );
+  }
+}
+
+export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
